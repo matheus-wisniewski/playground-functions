84cc369a0d810b98f774cb64e7f897f8cb2d6f3b
Matheus <maahwiski@gmail.com>
2022-09-27 17:11:48 -0300

    resolução desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -37,13 +37,37 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let repeat = 0;
+  let lastIndex = 0;
+  
+  for(let index = 0; index < array.length; index += 1){
+    lastIndex = array[index]
+
+    if(array[index] > lastIndex || array[index] === lastIndex){
+      repeat += 1;
+    }
+  } 
+  return repeat;
 }
 
+// console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
+
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanceCat1 = cat1 - mouse;
+  let distanceCat2 = cat2 - mouse;
+  let position;
+  // Transformar o valor da variável em positivo. Só usei em uma variável pq era a única dando esse erro.
+  distanceCat1 = Math.abs(distanceCat1);
+
+  if(distanceCat1 > distanceCat2){
+    position = 'cat2';
+  }else if(distanceCat2 > distanceCat1){
+    position = 'cat1';
+  }else if(distanceCat1 == distanceCat2){
+    position = 'os gatos trombam e o rato foge';
+  } return position
 }
 
 // Desafio 8
